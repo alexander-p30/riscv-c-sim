3afa7ebd0a7cd287798b8494b0a641848ffd3ab4
alexander-p30 <alexanderp30e@gmail.com>
2021-08-02 21:16:24 -0300

    fix: byte address validation

--- src/address_utils.c
+++ src/address_utils.c
@@ -9,18 +9,8 @@ int validateWordAddress(int32_t address) {
 
 int validateByteAddress(int32_t address) {
   uint8_t isAddressNatural = address >= 0;
-  uint8_t isAddressInsideMemSize = address <= MEM_SIZE;
+  uint8_t isAddressInsideMemSize = address <= MEM_SIZE * 4;
 
   return isAddressNatural && isAddressInsideMemSize;
-}
-
-int test(int32_t a) {
-  if(validateWordAddress(a)) {
-    printf("%d true\n", a);
-  } else {
-    printf("%d false\n", a);
-  }
-
-  return validateWordAddress(a);
 }
 
