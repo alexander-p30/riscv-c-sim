de378976cf9d003b603e4f5fbffdf6075aeb4ff9
alexander-p30 <alexanderp30e@gmail.com>
2021-08-14 13:57:18 -0300

    wip: rv32 state machine

--- src/riscv_instructions.c
+++ src/riscv_instructions.c
@@ -14,9 +14,15 @@ int32_t opcode = 0x0;
 int32_t rs1 = 0x0;
 int32_t rs2 = 0x0;
 int32_t rd = 0x0;
-int32_t shamt = 0x0;
 int32_t funct3 = 0x0;
 int32_t funct7  = 0x0;
+int32_t shamt = 0x0;
+
+int32_t imm12_i = 0x0;
+int32_t imm12_s = 0x0;
+int32_t imm13 = 0x0;
+int32_t imm20_u = 0x0;
+int32_t imm21 = 0x0;
 
 enum Registers {
   ZERO = 0, RA = 1, SP = 2, GP = 3, TP = 4, T0 = 5,
@@ -27,11 +33,7 @@ enum Registers {
   T4 = 29, T5 = 30, T6 = 31
 };
 
-int32_t breg[32] = {
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0
-};
+int32_t breg[32] = { };
 
 void fetch() {
   ri = lw(pc, 0);
@@ -45,17 +47,202 @@ void decode() {
   const int32_t rs1_mask = 0xF8000;
   const int32_t rs2_mask = 0x1F00000;
   const int32_t funct7_mask = 0xFE000000;
+
+
+
+  rd = (ri & rd_mask) >> OPCODE_SIZE;
+  funct3 = (ri & funct3_mask) >> (OPCODE_SIZE + RD_SIZE);
+  funct7 = (ri & funct7_mask) >> (OPCODE_SIZE + RD_SIZE + FUNCT3_SIZE + RS1_SIZE + RS2_SIZE);
+  rs1 = (ri & rs1_mask) >> (OPCODE_SIZE + RD_SIZE + FUNCT3_SIZE);
+  rs2 = (ri & rs2_mask) >> (OPCODE_SIZE + RD_SIZE + FUNCT3_SIZE + RS1_SIZE);
+
+  ri = 0b11111111111111111111111110000000 | OPCODE_I_0;
+  opcode = ri & opcode_mask;
+  imm12_i = get_imm();
+
+  ri = 0b11111111111111111111111110000000 | OPCODE_S;
+  opcode = ri & opcode_mask;
+  imm12_s = get_imm();
+
+  ri = 0b11111111111111111111111110000000 | OPCODE_B;
+  opcode = ri & opcode_mask;
+  imm13 = get_imm();
+
+  ri = 0b11111111111111111111111110000000 | OPCODE_U_0;
+  opcode = ri & opcode_mask;
+  imm20_u = get_imm();
+
+  ri = 0b11111111111111111111111110000000 | OPCODE_J;
+  opcode = ri & opcode_mask;
+  imm21 = get_imm();
+}
+
+int32_t get_imm() {
   const int32_t imm12_i_mask = 0xFFF00000;
   const int32_t imm12_s_mask = 0xFE000F80;
-  const int32_t imm13 = 0xFE000F80;
+  const int32_t imm13_mask = 0xFE000F80;
   const int32_t imm20_u_mask = 0xFFFFF000;
-  const int32_t imm21 = 0xFFFFF000;
+  const int32_t imm21_mask = 0xFFFFF000;
 
-  opcode = ri & opcode_mask;
-  rd = ri & rd_mask;
-  funct3 = ri & funct3_mask;
-  funct7 = ri & funct7_mask;
-  rs1 = ri & rs1_mask;
-  rs2 = ri & rs2_mask;
+  int32_t msb = 0x0;
+  int32_t second_msb = 0x0;
+  int32_t upper_part = 0x0;
+  int32_t eleventh_bit = 0x0;
+  int32_t lower_part = 0x0;
+
+  switch(opcode) {
+  case OPCODE_U_0:
+    return ri & imm20_u_mask;
+  case OPCODE_U_1:
+    return ri & imm20_u_mask;
+  case OPCODE_I_0:
+    return (ri & imm12_i_mask) >> (OPCODE_SIZE + RD_SIZE + FUNCT3_SIZE + RS1_SIZE);
+  case OPCODE_I_1:
+    return (ri & imm12_i_mask) >> (OPCODE_SIZE + RD_SIZE + FUNCT3_SIZE + RS1_SIZE);
+  case OPCODE_I_2:
+    return (ri & imm12_i_mask) >> (OPCODE_SIZE + RD_SIZE + FUNCT3_SIZE + RS1_SIZE);
+  case OPCODE_I_3:
+    return (ri & imm12_i_mask) >> (OPCODE_SIZE + RD_SIZE + FUNCT3_SIZE + RS1_SIZE);
+  case OPCODE_S:
+    upper_part = ((ri & 0xFE000000) >> (RS2_SIZE + RS1_SIZE + FUNCT3_SIZE + RD_SIZE + 2));
+    lower_part = ((ri & 0x00000F80) >> (OPCODE_SIZE));
+    return  (upper_part | lower_part);
+  case OPCODE_B:
+    msb = (ri & 0x80000000) >> 31 - 12;
+    second_msb = (ri & 0x00000080) << 4;
+    upper_part = (ri & 0x7E000000) >> ((RS2_SIZE  + RS1_SIZE + FUNCT3_SIZE + RD_SIZE + OPCODE_SIZE) - 2 - 3);
+    lower_part = (ri & 0xF00) >> OPCODE_SIZE;
+    return (msb | second_msb | upper_part | lower_part);
+  case OPCODE_J:
+    msb = (ri & 0x80000000) >> 11;
+    upper_part = (ri & 0x000FF000);
+    eleventh_bit = (ri & 0x00100000) >> 9;
+    lower_part = (ri & 0x7FE00000) >> 20;
+    return (msb | upper_part | eleventh_bit | lower_part);
+  }
 }
+
+void execute() {
+  switch(opcode) {
+    case OPCODE_U_0:
+      execute_U();
+    case OPCODE_U_1:
+      execute_U();
+    case OPCODE_I_0:
+      execute_I();
+    case OPCODE_I_1:
+      execute_I();
+    case OPCODE_I_2:
+      execute_I();
+    case OPCODE_I_3:
+      execute_I();
+    case OPCODE_R:
+      execute_R();
+    case OPCODE_S:
+      execute_S();
+    case OPCODE_B:
+      execute_B();
+    case OPCODE_J:
+      execute_J();
+  }
+}
+
+void execute_U() {
+  switch(opcode) {
+    case OPCODE_U_0: // lui
+     breg[rd] = imm20_u;
+    case OPCODE_U_1: // auipc
+     breg[rd] = pc + imm20_u;
+  }
+}
+
+void execute_I() {
+  switch(opcode) {
+    case OPCODE_I_0:
+      switch(funct3) {
+        case 0b000: // lb
+          breg[rd] = lb(rs1, imm12_i);
+        case 0b010: // lw
+          breg[rd] = lw(rs1, imm12_i);
+        case 0b100: // lbu
+          breg[rd] = lbu(rs1, imm12_i);
+      }
+    case OPCODE_I_1:
+      switch(funct3) {
+        case 0b000: // addi
+          breg[rd] = breg[rs1] + imm12_i;
+        case 0b110:
+          ori();
+        case 0b111:
+          andi();
+        case 0b001:
+          slli();
+        case 0b101:
+          switch(funct7) {
+            case 0b0000000:
+              srli();
+            case 0b0100000:
+              srai();
+          }
+      }
+    case OPCODE_I_2:
+      ecall();
+    case OPCODE_I_3:
+      jalr();
+  }
+}
+
+void execute_R() {
+  switch(funct3) {
+    case 0b000:
+      switch(funct7) {
+        case 0b00000000:
+          add();
+        case 0b01000000:
+          sub();
+      }
+    case 0b010:
+      slt();
+    case 0b011:
+      sltu();
+    case 0b100:
+      xor();
+    case 0b110:
+      or();
+    case 0b111:
+      and();
+  }
+}
+
+void execute_S() {
+  switch(funct3) {
+    case 0b000:
+      sb();
+    case 0b010:
+      sw();
+  }
+}
+
+void execute_B() {
+  switch(funct3) {
+    case 0b000:
+      beq();
+    case 0b001:
+      bne();
+    case 0b100:
+      blt();
+    case 0b101:
+      bge();
+    case 0b110:
+      bltu();
+    case 0b111:
+      bgeu();
+  }
+}
+
+void execute_J() {
+  jal();
+}
+
+
 
--- src/riscv_instructions.h
+++ src/riscv_instructions.h
@@ -3,11 +3,34 @@
 
 #include <stdint.h>
 
+#define OPCODE_U_0 0x37
+#define OPCODE_U_1 0x17
+#define OPCODE_I_0 0x3
+#define OPCODE_I_1 0x13
+#define OPCODE_I_2 0x73
+#define OPCODE_I_3 0x67
+#define OPCODE_R 0x33
+#define OPCODE_S 0x23
+#define OPCODE_B 0x63
+#define OPCODE_J 0x6F
+
+#define OPCODE_SIZE 7
+#define RD_SIZE 5
+#define FUNCT3_SIZE 3
+#define RS1_SIZE 5
+#define RS2_SIZE 5
+
 void fetch();
 
 void decode();
 
 void execute();
+void execute_U();
+void execute_I();
+void execute_R();
+void execute_S();
+void execute_B();
+void execute_J();
 
 void step();
 
@@ -16,5 +39,7 @@ void run();
 void dump_mem(int start, int end, char format);
 
 void dump_reg(char format);
+
+int32_t get_imm();
 
 #endif
